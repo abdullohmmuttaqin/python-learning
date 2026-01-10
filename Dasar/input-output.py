3cb4eeac156d92e5387721ce696f336f765edad0
Abd Muttaqin <abdullohmmuttaqin.09@gmail.com>
2026-01-11 03:29:37 +0700

    menambahkan kode baru di input-output.py

--- Dasar/input-output.py
+++ Dasar/input-output.py
@@ -1,10 +1,18 @@
+# nama_mahasiswa = input("Masukan nama mahasiswa: ")
+# umur = input(input("Masukan umur mahasiswa: "))
+
+# if umur <= 2026:
+#     status = "Umur anda adalah: "
+# else: status >= 2026
+#     status = "Umur anda adalah"
+
+# print("Nama mahasiwa: ", nama_mahasiswa)
+# print("Umur anda adalah: ", umur)
+
 nama_mahasiswa = input("Masukan nama mahasiswa: ")
-umur = input(input("Masukan umur mahasiswa: "))
+tahun_lahir = int(input("Masukan tahun lahir mahasiswa: "))
 
-if umur <= 2026:
-    status = "Umur anda adalah: "
-else: status >= 2026
-    status = "Umur anda adalah"
+umur = 2026 - tahun_lahir
 
-print("Nama mahasiwa: ", nama_mahasiswa)
-print("Umur anda adalah: ", umur)
+print("Nama mahasiswa:", nama_mahasiswa)
+print("Umur:", umur, "tahun")
