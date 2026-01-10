e011b58ec91d74e1743092e8bffd886d713d3c9e
Abd Muttaqin <abdullohmmuttaqin.09@gmail.com>
2026-01-11 03:21:41 +0700

    membuat program input-output

---
+++ Dasar/input-output.py
@@ -0,0 +1,10 @@
+nama_mahasiswa = input("Masukan nama mahasiswa: ")
+umur = input(input("Masukan umur mahasiswa: "))
+
+if umur <= 2026:
+    status = "Umur anda adalah: "
+else: status >= 2026
+    status = "Umur anda adalah"
+
+print("Nama mahasiwa: ", nama_mahasiswa)
+print("Umur anda adalah: ", umur)
