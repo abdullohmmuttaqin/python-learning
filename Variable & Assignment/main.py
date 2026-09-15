ab6b5a636d991869b7731716986c26ec59e566fc
abdullohmmuttaqin <abdullohmmuttaqin.09@mail.com>
2026-09-15 19:02:27 +0700

    Feat/Variable & Assignment

---
+++ Variable & Assignment/main.py
@@ -0,0 +1,5 @@
+greeting = "Adili Jokowi secepatnya!" #this is assignment
+
+print (greeting)
+
+# Pada kode di atas, teks “Adili Jokowi secepatnya!” disimpan pada variabel “greeting”. Jadi, setelah proses assignment tersebut, variabel “greeting” memiliki nilai “Adili Jokowi secepatnya!”
